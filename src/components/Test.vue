8e8368ec395922db6563f0f39c3198c054d93e53
Brendan Mulholland <brendan@recitalapp.com>
2024-08-02 16:04:41 +0200

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  methods: {
+    test(): void {
+      this.$apollo
+    }
+  }
+})
+</script>
+
+<template>
+  <div></div>
+</template>
