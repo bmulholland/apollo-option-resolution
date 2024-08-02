e5b6d07167bfdf4772d832d1109fab91bfe99e0b
Brendan Mulholland <brendan@recitalapp.com>
2024-08-02 18:38:51 +0200

    less component

--- test.ts
+++ test.ts
@@ -2,9 +2,7 @@ import { createApolloProvider } from "@vue/apollo-option";
 import { defineComponent } from "vue";
 
 defineComponent({
-  methods: {
-    test(): void {
-      this.$apollo;
-    },
+  setup() {
+    this.$apollo;
   },
 });
