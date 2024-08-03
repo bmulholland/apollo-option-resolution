b746f2a68277d670b9fe7472769417120c71b4c6
Brendan Mulholland <brendan@recitalapp.com>
2024-08-03 17:14:39 +0200

    even less

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,6 @@
 import { DollarApollo } from "./vue-apollo";
 
 declare module "@vue/runtime-core" {
-  interface ComponentOptionsBase<
-    Props,
-    RawBindings,
-    D,
-    C extends ComputedOptions,
-    M extends MethodOptions,
-    Mixin extends ComponentOptionsMixin,
-    Extends extends ComponentOptionsMixin,
-    E extends EmitsOptions,
-    EE extends string = string,
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    Defaults = {},
-  > {
-    apollo?: object;
-  }
-
   interface ComponentCustomProperties {
     $apollo: DollarApollo<this>;
   }
