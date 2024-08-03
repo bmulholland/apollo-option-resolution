689bd056416a96b656ebc129c09a7e0836c2abfd
Brendan Mulholland <brendan@recitalapp.com>
2024-08-03 16:55:34 +0200

    simplify types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,41 @@
-import './vue'
-import { ApolloProvider, ApolloProviderOptions } from './apollo-provider'
+import { DollarApollo } from "./vue-apollo";
+import { VueApolloComponentOptions } from "./options";
+import { ApolloProvider } from "./apollo-provider";
 
-export { ApolloProvider }
-export function createApolloProvider (options: ApolloProviderOptions): ApolloProvider
+declare module "@vue/runtime-core" {
+  interface ComponentOptionsBase<
+    Props,
+    RawBindings,
+    D,
+    C extends ComputedOptions,
+    M extends MethodOptions,
+    Mixin extends ComponentOptionsMixin,
+    Extends extends ComponentOptionsMixin,
+    E extends EmitsOptions,
+    EE extends string = string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    Defaults = {},
+  > {
+    apolloProvider?: ApolloProvider;
+    apollo?: VueApolloComponentOptions<
+      CreateComponentPublicInstance<
+        Props,
+        RawBindings,
+        D,
+        C,
+        M,
+        Mixin,
+        Extends,
+        E,
+        Props,
+        Defaults,
+        false
+      >
+    >;
+  }
+
+  interface ComponentCustomProperties {
+    $apolloProvider: ApolloProvider;
+    $apollo: DollarApollo<this>;
+  }
+}
